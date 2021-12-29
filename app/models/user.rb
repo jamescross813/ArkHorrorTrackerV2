f9aa3b68aa6b30c68cddf30c040250e8eacda800
jamescross813 <crossjames813@gmail.com>
2021-12-29 14:57:10 -0600

    user model added

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    has_many :games
+    has_secure_password
+
+    validates :username, uniqueness: true, presence: true
+    validates :password_digest, presence:true
+
+end
