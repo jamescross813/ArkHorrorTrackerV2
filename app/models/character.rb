8fee0779cf35d3bceebbc912c2ddb4bb39ff3267
jamescross813 <crossjames813@gmail.com>
2021-12-29 14:54:10 -0600

    character model added

---
+++ app/models/character.rb
@@ -0,0 +1,7 @@
+class Character < ApplicationRecord
+  belongs_to :game
+  belongs_to :character_base
+
+  validates :health, :sanity, :victory_points_gained, :victory_points_used, numericality: { only_integer: true, message: "Must be a number"}
+  
+end
